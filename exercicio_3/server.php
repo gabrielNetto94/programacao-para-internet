53b5b9216e3a70acec17b304178dcff449aac13c
Gabriel Gregório Silveira Netto <33572929+gabrielNetto94@users.noreply.github.com>
2020-03-21 16:36:54 -0300

    finalização exercicio 3

---
+++ exercicio_3/server.php
@@ -0,0 +1,44 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+
+	$name = $_POST['name'];
+	$date = $_POST['date'];
+	$gender = $_POST['gender'];
+	$course = $_POST['course'];
+	$suggestion = $_POST['suggestion'];
+	$checkbox = '';
+
+	$flag = false;
+	 if (isset($_POST['checkbox'])){
+	 	$checkbox = $_POST['checkbox'];
+	 	$flag = true;
+	 }
+
+
+	echo "Nome: ".$name."\n";
+	echo "Data: ".$date."\n";
+	echo "Itens checkbox: "; 
+	if($flag){
+		showCheckBox($checkbox);
+	}
+	echo "\nGênero: ".$gender."\n";
+	echo "Curso: ".$course."\n";
+	echo "Sugestão: ".$suggestion;
+
+
+
+	function showCheckBox($checkItens){
+		foreach ($checkItens as &$item) {
+			echo $item."\t";
+		}
+
+	}
+	/*if(isset($_POST['name']) && isset($_POST['date']) ){
+        
+	}
+	
+	else{
+		echo "Nenhum parâmetro passado, nem por GET e nem por POST";
+	}*/
+?>
