5a718a00df7c13bcb36d617462c9bde1890020db
Gabriel Gregório Silveira Netto <33572929+gabrielNetto94@users.noreply.github.com>
2020-04-08 14:48:49 -0300

    finalização cálculo do buffet, gravação dos dados em arquivo

--- prova/backend/register_guests.php
+++ prova/backend/register_guests.php
@@ -4,25 +4,65 @@
     $numberGuests = $_POST['number-guests'];
     $dessert = $_POST['dessert'];
 
-    $budgetValue = $numberGuests * 22.9; //calculo comida
+    $valueBuffet = 22.90;
+
+    $budgetValue = $numberGuests * $valueBuffet; //calculo comida
+    $valueGuests = $budgetValue;
 
     if($dessert == 'yes'){
+        $txDessert = $budgetValue * 0.1;
         $budgetValue = $budgetValue * 1.1;
+        
     }
 
     $numberWaiter = $numberGuests / 15;
     
     $numberWaiter = ceil($numberWaiter);
-    //$numberWaiter = intval($numberWaiter);
     
     $totalValueWaiter = $numberWaiter * 250;
     
     $budgetValue = $budgetValue +  $totalValueWaiter;
     
-    echo $numberGuests."\n";
-    echo "PRATO X CONVIDADOS ".$budgetValue."\n";
-    echo "Número de garçons: ".$numberWaiter."\n";
-    echo "Valor Garçons: ".$totalValueWaiter."\n";
-    echo "Valor do orçamento: ".$budgetValue."\n";
+    $fileName = "budgets/".$name.".txt";
+
+    $file = fopen($fileName,"w");
+
+    fwrite($file,$name."\n");
+    fwrite($file,$numberGuests."\n");
+    fwrite($file,$valueBuffet."\n");
+    fwrite($file,$valueGuests."\n");
+    if($dessert == 'yes'){
+        fwrite($file,$txDessert."\n");
+     }
+    fwrite($file,$numberWaiter."\n");
+    fwrite($file,$totalValueWaiter."\n");
+    fwrite($file,$budgetValue."\n");
+
+    fclose($file);
 
 ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Orçamento</title>
+    </head>
+    
+    <body>
+        <h4>Nome do Cliente: <?php echo $name ?></h4>
+        <h4>Número de convidados: <?php echo $numberGuests ?></h4>
+        <h4>Valor por convidado: <?php echo "R$ ".$valueBuffet ?> </h4>
+        <h4>Valor total por convidados: <?php echo "R$ ".$valueGuests ?> </h4>
+        <?php if($dessert == 'yes'){
+            echo "<h4>Taxa de sobremesa: R$ $txDessert</h4>";
+         }?>
+        <h4>Quantidade de garçons: <?php echo $numberWaiter ?></h4>
+        <h4>Taxa de garçons: <?php echo "R$ ".$totalValueWaiter ?> </h4>
+        <h4>Valor do orçamento: <?php echo "R$ ".$budgetValue ?></h4>
+
+    </body>
+</html>
+
+
+No momento de exibir o resultado, o sistema deverá apresentar as informações em um layout de uma proposta comercial, 
+contendo os dados da empresa (inventar informações fictícias, juntamente de uma imagem para a empresa), os dados do cliente,
+bem como os valores da proposta (valor total por convidado, taxa de sobremesa, quantidade de garçons e taxa de garçons)
